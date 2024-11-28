98fff42d6804d16c49cb5ea581cd4cd2981b4a2c
Jose Villa <villa.herrera.1994@gmail.com>
2024-11-28 18:56:02 -0300

    se agrego el proceso mutuo de las dos categorias de manera secuencial

--- index.py
+++ index.py
@@ -3,26 +3,50 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+from openpyxl import Workbook
+from datetime import datetime
+
+from scrapdata import ScrapData
 
 class Index:
-    def __init__(self):
+    def __init__(self, ruta, ws):
         # URL base de las páginas de productos
-        self.ruta = 'https://www.wrangler.cl/mujer?layout=option_4_items&order=OrderByReleaseDateDESC&page='
+        self.ruta = ruta
+        # Hoja activa de Excel compartida
+        self.ws = ws
         # Configuración de Selenium WebDriver
         self.driver = webdriver.Chrome()  # Asegúrate de tener el ChromeDriver instalado
+        self.arrayLinks = []
 
+    # Procesar los datos de los productos y agregar al archivo Excel
+    def process_data(self):
+        categoria = self.ruta.split('/')[3].split('?')[0]
+
+        for i in range(0, len(self.arrayLinks)):  # Iterar sobre los enlaces recopilados
+            scrapData = ScrapData(self.arrayLinks[i], self.driver, categoria)  # Crear la instancia de ScrapData
+            data = scrapData.get_data()
+
+            if data and len(data) >= 2 and data[0] and data[1]:
+                # Agregar los datos a la hoja Excel compartida
+                self.ws.append([data[0], data[1], categoria])
+            else:
+                print(f"Datos inválidos o incompletos para el enlace: {self.arrayLinks[i]}")
+
+    # Obtener los datos de los productos de la página
     def get_data(self, soup):
         # Buscar los contenedores de productos
         products = soup.find_all('div', class_='vtex-search-result-3-x-galleryItem')
-        print(f"Se encontraron {len(products)} productos.")
-        # Imprimir los productos encontrados (puedes extraer más información si lo deseas)
+
         for product in products:
             link = product.find('a', class_='vtex-product-summary-2-x-clearLink')['href']
             finishLink = f'https://www.wrangler.cl{link}'
-            print(finishLink)
 
+            if finishLink not in self.arrayLinks:
+                self.arrayLinks.append(finishLink)
+
+    # Navegar por las páginas de productos
     def numberPages(self):
-        for i in range(1, 20):  # Ajusta el rango de páginas que deseas cargar
+        for i in range(1, 100):  # Ajusta el rango de páginas que deseas cargar
             url = self.ruta + str(i)
             self.driver.get(url)
             
@@ -36,15 +60,57 @@ class Index:
                 html = self.driver.page_source
                 soup = BeautifulSoup(html, 'html.parser')
                 self.get_data(soup)
+
             except Exception as e:
                 print(f"Error al cargar la página {i}: {e}")
                 break
+        
+        self.process_data()
 
     def close_driver(self):
         # Cerrar el navegador después de terminar
         self.driver.quit()
 
-# Crear instancia y ejecutar
-index = Index()
-index.numberPages()
-index.close_driver()
+# Registrar el tiempo de inicio
+horaStart = datetime.now()
+print(f'Proceso Iniciado a las {horaStart}')
+
+# Crear un archivo Excel único
+wb = Workbook()
+ws = wb.active
+ws.title = "Productos"
+
+# Escribir los encabezados en la primera fila
+ws.append(["Código", "Nombre", "Categoría"])
+
+# Lista de URLs a procesar
+listRuta = [
+    'https://www.wrangler.cl/hombre?layout=option_4_items&order=OrderByReleaseDateDESC&page=',
+    'https://www.wrangler.cl/mujer?layout=option_4_items&order=OrderByReleaseDateDESC&page=',
+]
+
+# Iterar sobre cada URL en la lista y ejecutar el proceso
+for ruta in listRuta:
+    index = Index(ruta, ws)  # Pasar la hoja activa como parámetro
+    index.numberPages()  # Cargar las páginas y procesar los datos
+    index.arrayLinks = []  # Limpiar la lista de enlaces
+
+index.close_driver()  # Cerrar el navegador después de procesar
+
+# Guardar el archivo Excel único
+fecha_hora_actual = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+archivo_excel = f'Productos_combinados_{fecha_hora_actual}.xlsx'
+wb.save(archivo_excel)
+
+# Registrar el tiempo de finalización
+horaEnd = datetime.now()
+print(f'Proceso completado a las {horaEnd}')
+
+# Calcular la duración del proceso
+timeDifference = horaEnd - horaStart
+hours = timeDifference.seconds // 3600
+minutes = (timeDifference.seconds % 3600) // 60
+seconds = timeDifference.seconds % 60
+
+print(f'Duración del proceso: {hours} horas, {minutes} minutos, {seconds} segundos')
+print(f'Datos guardados en el archivo: {archivo_excel}')
--- scrapdata.py
+++ scrapdata.py
@@ -8,71 +8,75 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 class ScrapData:
-    def __init__(self, url='https://www.wrangler.cl/jeans-hombre-texas-regular-fit-stonewash/p'):
+    def __init__(self, url='https://www.wrangler.cl/jeans-hombre-texas-regular-fit-stonewash/p', driver=None, categoria=''):
         self.url = url
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
+        self.driver = driver
+        self.categoria = categoria
 
     def descargar_imagen(self, link_img, nombre_archivo):
-        # Verificar si la carpeta "Descargas" existe, si no, crearla
+        # Ruta de la carpeta Descargas/categoria
         carpeta_descargas = "Descargas"
+        carpeta_categoria = os.path.join(carpeta_descargas, self.categoria)
+
+        # Verificar si la carpeta "Descargas" existe, si no, crearla
         if not os.path.exists(carpeta_descargas):
             os.makedirs(carpeta_descargas)
             print(f'Carpeta "{carpeta_descargas}" creada.')
 
-        # Crear la ruta completa del archivo
-        ruta_archivo = os.path.join(carpeta_descargas, nombre_archivo)
+        # Verificar si la carpeta "Descargas/categoria" existe, si no, crearla
+        if not os.path.exists(carpeta_categoria):
+            os.makedirs(carpeta_categoria)
+            print(f'Carpeta "{carpeta_categoria}" creada.')
+
+        # Ruta completa del archivo
+        ruta_archivo = os.path.join(carpeta_categoria, nombre_archivo)
 
         try:
-            # Realizar la solicitud para obtener el contenido de la imagen
             response = requests.get(link_img)
-            
-            # Verificar si la solicitud fue exitosa (código de estado 200)
             if response.status_code == 200:
-                # Guardar la imagen en el directorio "Descargas"
                 with open(ruta_archivo, 'wb') as file:
                     file.write(response.content)
                 print(f'Imagen descargada correctamente en {ruta_archivo}')
             else:
                 print(f'Error al descargar la imagen. Estado: {response.status_code}')
-        
         except Exception as e:
             print(f'Ocurrió un error al descargar la imagen: {e}')
 
-
     def get_data(self):
-        self.driver.get(self.url)
-
         try:
-            # Esperar a que el nombre del producto esté visible (usando XPath)
+            self.driver.get(self.url)
+
+            # Extraer el nombre del producto
+            nombre_modificado = 'Nombre no encontrado'
             nombre_element = WebDriverWait(self.driver, 10).until(
                 EC.visibility_of_element_located((By.XPATH, '//h3[contains(@class, "wranglercl-complement-name-0-x-productName--pdp-product-name")]'))
             )
-            
-            # Extraer el nombre del producto
-            nombre = nombre_element.text
-            nombre_modificado = nombre.split(' ')[0] + ' ' + ' '.join([palabra.lower() for palabra in nombre.split(' ')[1:]])
+            if nombre_element:
+                nombre = nombre_element.text
+                nombre_modificado = nombre.split(' ')[0] + ' ' + ' '.join(
+                    [palabra.lower() for palabra in nombre.split(' ')[1:]]
+                )
 
-            # Extraer el código del producto usando el XPath correcto
-            codigo_element = self.driver.find_element(By.XPATH, '//div[contains(@class, "wranglercl-complement-name-0-x-container_identifier")]/p[contains(@class, "wranglercl-complement-name-0-x-identifier--pdp-product-identifier")]')
-            codigo = codigo_element.text.strip() if codigo_element else 'Código no encontrado'
-            codigo_extraido = codigo.split(" / ")[-1]  # Dividimos la cadena por " / " y tomamos la segunda parte
-            
-            
-            # Esperar a que la imagen esté presente en el DOM
+            # Extraer el código del producto
+            codigo_extraido = 'Código no encontrado'
+            codigo_element = self.driver.find_element(
+                By.XPATH, '//div[contains(@class, "wranglercl-complement-name-0-x-container_identifier")]/p[contains(@class, "wranglercl-complement-name-0-x-identifier--pdp-product-identifier")]'
+            )
+            if codigo_element:
+                codigo = codigo_element.text.strip()
+                codigo_extraido = codigo.split(" / ")[-1]
+
+            # Descargar la imagen
+            link_img = 'Imagen no encontrada'
             img_element = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.XPATH, '//img[contains(@class, "vtex-store-components-3-x-productImageTag")]'))
             )
-            link_img = img_element.get_attribute('src') if img_element else 'Imagen no encontrada'
-            
-            # Descargar la imagen
-            self.descargar_imagen(link_img, f'{codigo_extraido}.jpg')
-            
+            if img_element:
+                link_img = img_element.get_attribute('src')
+                self.descargar_imagen(link_img, f'{codigo_extraido}.jpg')
+
             return [codigo_extraido, nombre_modificado]
 
-            
-
         except Exception as e:
-            print(f'Error al extraer datos: {e}')
-        finally:
-            self.driver.quit()
+            print(f"Error en el método get_data: {e}")
+            return ['Código no encontrado', 'Nombre no encontrado']
